18fc122b102e1dce8f605b5a45541b5c62f622c2
Molly <mojeanmac@gmail.com>
2022-02-04 16:25:50 -0800

    Added Tester

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,8 @@
-public class SkillDemo{}
+public class SkillDemo{
+    public static boolean checkX(int x){
+        if (x == 5){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void checkX(){
+        assertFalse(SkillDemo.checkX(5));
+    }
+}
